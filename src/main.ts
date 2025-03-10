ca8e53f7a4f95f1ff6902fce25be52ed9841a7b5
Prataad <pondaeroz008y@gmail.com>
2025-03-10 10:33:57 +0700

    feat: add config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+    console.log("server is listening on port", process.env.SERVER_PORT);
+  });
 }
 bootstrap();
