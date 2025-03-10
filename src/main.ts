e472504244644e27fbb49a79fddaeeb78a389563
Prataad <pondaeroz008y@gmail.com>
2025-03-10 10:44:15 +0700

    feat: add class validator and class transformer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      stopAtFirstError: true,
+    }),
+  );
+  
   await app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
     console.log("server is listening on port", process.env.SERVER_PORT);
   });
